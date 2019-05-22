0d38377e17d78aadc436b672e71092f66be36bb2
Bartek Rybak <gotujcygana@gmail.com>
2019-05-22 04:22:03 -0700

    Add files via upload

---
+++ GmailClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Mail;
+using System.Net;
+
+namespace MailSender
+{
+	public class GmailClient
+	{
+		NetworkCredential LoginCredential;
+		SmtpClient Client;
+
+		int gmail_port = 587;
+		string gmail_host = "mtp.gmail.com";
+
+		public GmailClient(string login,string password)
+		{
+			LoginCredential = new NetworkCredential(login, password);
+		}
+
+		public void Send(MailMessage mail)
+		{
+			Client = new SmtpClient
+			{
+				Host = gmail_host,
+				Port = gmail_port,
+				UseDefaultCredentials = false,
+				Credentials = LoginCredential,
+				EnableSsl = true,
+				DeliveryMethod = SmtpDeliveryMethod.Network
+			};
+
+			try
+			{
+				Client.Send(mail);
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+		}
+	}
+}
